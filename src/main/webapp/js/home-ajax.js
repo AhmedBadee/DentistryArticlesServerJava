6ef84598bb6ff793a1e27de78c173292069f2f45
AhmedBadee <ahmedbadee17@gmail.com>
2017-12-05 23:56:46 +0200

    parsing the ajax response and formatting the display of the articles

--- src/main/webapp/js/home-ajax.js
+++ src/main/webapp/js/home-ajax.js
@@ -1,12 +1,20 @@
-$(document).ready(function () {
-    var rootURL = '/DentistryArticles/REST/';
+$(document).ready(function() {
+    var rootURL = '/DentistryArticlesMaven/REST/';
 
     $.ajax({
         type: 'GET',
         url : rootURL + 'Articles/',
         dataType: 'json',
         success : function(result) {
-            $('#result').html(result);
+            // $('#result').html(JSON.stringify(result));
+            var articles = JSON.parse(JSON.stringify(result));
+            for (i = 0; i < articles.length; i++) {
+                $('#articles').append('<div id="article">'
+                                  + '<h2>' + articles[i].articleTitle + '</h2>'
+                                  + '<h3>' + articles[i].articleBrief + '</h3>'
+                                  + '<a href="'+ rootURL + 'Articles/' + articles[i].articleId + '" >Read Full Article</a>'
+                                  + '</div>');
+            }
         }
     });
 });
